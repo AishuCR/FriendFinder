0c066feaf87247be8903d23e7e710a990c0d6f4b
Aishu <crishwarya@gmail.com>
2018-08-08 11:06:24 -0700

    some changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,36 @@
 //Routes API
+var path =require('path');
 var friends = require('../data/friends.js');
 
 module.exports = function (app) {
   app.get("/api/friends", function (req, res) {
     res.json(friends);
   });
+
   app.post("/api/friends", function (req, res) {
 var userInput = req.body;
-for (var i = 0; i <userInput.scores.length; i++){
-  userInput.scores[i] = parseInt(userInput.scores[i]);
-}
-var bestFriendIndex = 0;
-var minimumDifference = 100;
-for(var i =0; i<friends.length; i++){
+var userResponse = userInput.scores;
+var match = {
+  name: "",
+  photo: "",
+  difference: 100
+};
+
+for (var i = 0; i <friends.length; i++){
+
 var totalDifference = 0;
-for(var j =0; j< friends[i].scores.length; j++){
-  var difference = Math.abs(userInput.scores[j] - friends[i].scores[j]);
-  totalDifference += diffrence;
+
+for(var j = 0; j< userResponse.length; j++){
+  totalDifference += Math.abs(friends[i].scores[j] - userResponse[j]);
 }
-if(totalDifference < minimumDifference){
-  bestFriendIndex = i;
-  minimumdifference = totalDifference;
+
+if(totalDifference <= match.difference){
+ match.name = friends[i].name;
+ match.photo = friends[i].photo
+  match.difference = totalDifference;
 }
 }
 friends.push(userInput);
-res.json(friends[bestFriendIndex]);
+res.json(match);
 });
 };
--- server.js
+++ server.js
@@ -7,11 +7,12 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/app/public"));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function(){
-    console.log('App listening on PORT: ' + PORT);
+    console.log('App listening on PORT:' + PORT);
 });
